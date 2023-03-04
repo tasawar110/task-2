b18db7c834ef38a5bddc5286740b5069ec4db459
tasawar110 <126941328+tasawar110@users.noreply.github.com>
2023-03-04 01:00:28 -0800

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,16 @@
+#task1
+a = (1,2,3,4,5)
+b = [5.6,6.7,7.8,9.8,2.2]
+#c = [1+3f,2-4r,3-4e,3+5k,6+9g] ye ksy store krna h
+print(a)
+print(b)
+#task 2
+print(type(a))
+print(type(b))
+#task3
+f=['tasawar','Father','mother','brother','sister']
+print(type(f))
+#task4
+Intro = ['hello myself tasawar','I am learning python from skillvention']
+print(Intro)
+print(type(Intro))
